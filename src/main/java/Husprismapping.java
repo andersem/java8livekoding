f0ef6bd4d5d03b302aa6d90c1eb2b3006018293f
Anders Emil Salvesen <andersems@gmail.com>
2016-11-13 19:22:43 +0100

    Mapping-oppgave

---
+++ src/main/java/Husprismapping.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class Husprismapping {
+    public static void main(String[] args) {
+        List<Integer> tall = Arrays.asList(1, 2, 3, 4, 5, 10, 20, 40);
+
+        // Boligprisene i Oslo m/ Bærum steg 12.7% fra 2. kvartal 2015 til 2. kvartal 2016
+        // Gitt en liste med priser på leiligheter i 2015, finn gjennomsnittsprisen
+        // i 2016
+
+
+    }
+}
