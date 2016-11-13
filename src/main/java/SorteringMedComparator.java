c2a507843fb9139d558d52f935ca1d6478530e1f
Anders Emil Salvesen <andersems@gmail.com>
2016-11-13 14:08:30 +0100

    Ferdig med sortering

--- src/main/java/SorteringMedComparator.java
+++ src/main/java/SorteringMedComparator.java
@@ -6,14 +6,27 @@ public class SorteringMedComparator {
     public static void main(String[] args) {
         List<Noe> listeMedNoe = Arrays.asList(new Noe("A"), new Noe("C"), new Noe("B"));
 
-
+        listeMedNoe.sort((o1, o2) -> o1.name.compareTo(o2.name));
+        System.out.println(listeMedNoe);
     }
 
-    public static class Noe {
-        public final String name;
+
+
+
+
+
+
+
+    private static class Noe {
+        final String name;
 
         Noe(String name) {
             this.name = name;
+        }
+
+        @Override
+        public String toString() {
+            return name;
         }
     }
 }
