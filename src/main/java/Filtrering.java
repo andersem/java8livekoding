3c9b3d7bad16e11385171c0d7504003ccbafacf0
Anders Emil Salvesen <andersems@gmail.com>
2016-11-13 17:23:24 +0100

    Oppgaver til filtrering

--- src/main/java/Filtrering.java
+++ src/main/java/Filtrering.java
@@ -7,13 +7,13 @@ public class Filtrering {
         List<String> liste = Arrays.asList(
                 "a1", "a2", "a3", "a4",
                 "b1", "b2", "b3",
-                "c1", "c2", "c3", "c4", "c4");
+                "c1", "c2", "c3", "c4", "c4", "c11");
 
         // Finn alle strenger i lista som begynner med a
         // Finn alle strenger i lista som er nr 1 for den bokstaven
+        // Finn første streng som inneholder 3
         // Fjern duplikater
         // Finn gjennomsnittlig antall per bokstav
-
 
 
     }
--- src/main/java/SorteringMedComparator.java
+++ src/main/java/SorteringMedComparator.java
@@ -5,9 +5,6 @@ public class SorteringMedComparator {
 
     public static void main(String[] args) {
         List<Noe> listeMedNoe = Arrays.asList(new Noe("A"), new Noe("C"), new Noe("B"));
-
-        listeMedNoe.sort((o1, o2) -> o1.name.compareTo(o2.name));
-        System.out.println(listeMedNoe);
     }
 
 
