1542ba2f2d7067a848ad78a0cf7ab229389b152e
Anders Emil Salvesen <andersems@gmail.com>
2016-11-13 14:21:41 +0100

    Begynner med filtrering

---
+++ src/main/java/Filtrering.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class Filtrering {
+
+    public static void main(String[] args) {
+        List<String> liste = Arrays.asList(
+                "a1", "a2", "a3", "a4",
+                "b1", "b2", "b3",
+                "c1", "c2", "c3", "c4", "c4");
+
+
+
+    }
+}
