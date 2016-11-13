9c0f49075622146d69c28edace3541582accb23d
Anders Emil Salvesen <andersems@gmail.com>
2016-11-13 14:25:26 +0100

    Begynner med filtrering, lagt til oppgaver

--- src/main/java/Filtrering.java
+++ src/main/java/Filtrering.java
@@ -9,6 +9,11 @@ public class Filtrering {
                 "b1", "b2", "b3",
                 "c1", "c2", "c3", "c4", "c4");
 
+        // Finn alle strenger i lista som begynner med a
+        // Finn alle strenger i lista som er nr 1 for den bokstaven
+        // Fjern duplikater
+        // Finn gjennomsnittlig antall per bokstav
+
 
 
     }
